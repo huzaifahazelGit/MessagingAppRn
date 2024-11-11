60f20b88cf3e9422e6d890b195fffb13cdeca7a9
Muhammad_Huzaifa_Junaid <huzaifajunaid.hazel@gmail.com>
2024-11-11 20:41:52 +0500

    Added boradcasting message

--- src/screens/marketplace/utils/market_data.tsx
+++
@@ -1,6 +0,0 @@
-export const more_list = [
-    { id: 1, text: 'RENEW',  },
-    { id: 2, text: 'ACTIVE', },
-  ];
-export const options_condition_tag = ['NEW', 'USED - LIKE NEW', 'USED - GOOD','USED',]; 
-export const payouts_options = ['USD','ETH','REALM COIN',]; 
--- src/screens/marketplace/utils/orders.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum ORDER_STATUS{
-    PENDING='pending',
-    APPROVED_BY_SELLER='approvedBySeller',
-    COMPLETED='completed'
-}
